528b1ccd7bc03aea7846f81d94e2023913a32d56
Rose Rozzy <rosecarenwangui.com>
2022-09-19 17:47:17 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _puts ->this is a function that puts
  * @str: a param to _puts function
+ * Description: prints a string
+ * On success: returns no error
  */
 
 void _puts(char *str)
 {
-	for (;str != '\0'; str++)
-	{ 
-		putchar(*str);
-        }  
-	putchar('\n');
+       int 1 = 0
+       while (*(str + i) != '\0')
+       {
+	       putchar(*(str + i));
+	       i++;
+       }
+       putchar(10);
 }

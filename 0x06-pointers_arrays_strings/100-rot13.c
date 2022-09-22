100e1fe188ca22dbf5102dd5f9fcb6ecb9a0203d
Rose Rozzy <rosecarenwangui.com>
2022-09-22 10:17:15 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * rot13 -  encodes a string using rot13
+ * @str:the string targeted
+ *Return: returns the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int index1, index2;
+
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+
+
+
+
+
+
+
+
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+
+
+
+
+
+
+
+
+	while (str[++index1])
+	{
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+		}
+	}
+	return (str);
+	}

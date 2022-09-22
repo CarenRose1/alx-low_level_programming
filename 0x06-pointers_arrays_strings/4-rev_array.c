346c2017bb1afafbff75bbe3eeac93520b906ee4
Rose Rozzy <rosecarenwangui.com>
2022-09-22 08:59:07 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,10 +6,11 @@
  * @n: number of elements
  */
 
-vouid reverse_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
 	int tmp, index;
-	for (index = n - 1; index > n /2; index --)
+
+	for (index = n - 1; index > n / 2; index--)
 	{
 	tmp = a[n - 1 - index];
 	a[n - 1 - index] = a[index];

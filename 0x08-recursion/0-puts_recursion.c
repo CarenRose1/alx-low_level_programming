d6f1b0b02480570f957b3702c5d6a05b5333d221
Rose Rozzy <rosecarenwangui.com>
2022-09-28 12:49:27 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,21 +3,17 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string
+ *
  * Return: void
  */
-
-
 void _puts_recursion(char *s)
 {
 
-	if (*s == '\0')
+	if (*s == '\0') /* base case */
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
+	_putchar(*s); 
+	_puts_recursion(s + 1);
 }

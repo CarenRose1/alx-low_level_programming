86dca6e015b3c38de07e528475f2e52afe195678
Rose Rozzy <rosecarenwangui.com>
2022-10-04 21:56:49 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,18 +8,26 @@
  */
 char *_strdup(char *str);
 {
-	char *pstr;
-	unsigned int i = 0;
+	char *duplicate;
+	int index, len = 0;
 
-	if (str == 0)
-		return (0);
-	while (str[i] != 0)
-		i++;
-	i++;
-	pstr = malloc(sizeof(char) * i);
-	for (i = 0; str[i] != 0; i++)
-		pstr[i] = str[i];
-	pstr[i] = 0;
-	return (pstr);
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
 

6c9285fdfab80ad8ea62f2f47bb4d2f120eb3af1
Rose Rozzy <rosecarenwangui.com>
2022-10-05 01:41:50 -0700

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,15 +1,16 @@
 #include "main.h"
+#include <sdlib.h>
 
 /**
  * free_grid -free a 2-dimensional array created by alloc_grid
  * @grid: a pointer to the first column of the first row of the array
  * @height: the number of rows in the array
- *
- * Return: void
  */
 void free_grid(int **grid, int height)
 {
-	while (height)
-		free(grid[--height]);
-	free(grid)
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }

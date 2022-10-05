d259cd52dd61853ff804fa7b7089be1703fddf00
Rose Rozzy <rosecarenwangui.com>
2022-10-05 01:36:33 -0700

    alloy rid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,4 +22,31 @@ int **alloc_grid(int width, int height);
 	{
 		free(a);
 		return (NULL);
-	}	
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+
+			free(a);
+			return (NULL);
+		}
+	}
+
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+
+	return (a);
+}
